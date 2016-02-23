ab3652833a20aba7a2d215ed29866b6ae2bf4326
sklirg <hlsolbjorg@gmail.com>
2016-02-22 22:16:56 -0400

    Add django-oauth-toolkit URLs.

--- microauth/urls.py
+++ microauth/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    # django-oauth-toolkit
+    url(r'^oauth2/', include('oauth2_provider.urls', namespace='oauth2_provider')),
 ]
