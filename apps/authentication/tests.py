2dfb29aef306321747901d92bd4a3bbc10b10759
sklirg <hlsolbjorg@gmail.com>
2016-02-25 00:03:47 -0400

    Add a test.
    This tests checks if we managed to load the root view of the web page without error (expecting HTTP 200 OK on load).

--- apps/authentication/tests.py
+++ apps/authentication/tests.py
@@ -1,3 +1,12 @@
-# from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.test import TestCase
 
-# Create your tests here.
+
+class HTTPGetRootTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_get_root_expect_http_200(self):
+        url = reverse('microauth_authentication:index')
+        response = self.client.get(url)
+        self.assertEqual(200, response.status_code, 'Expect root view to load without issues.')
