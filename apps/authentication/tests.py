176c03e26f46bad73df39c11ea4a190baca6fe54
sklirg <hlsolbjorg@gmail.com>
2016-02-25 01:16:59 -0400

    Make test not depend on django-pipeline

--- apps/authentication/tests.py
+++ apps/authentication/tests.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.test.utils import override_settings
 
 
 class HTTPGetRootTestCase(TestCase):
@@ -7,6 +9,9 @@ class HTTPGetRootTestCase(TestCase):
         pass
 
     def test_get_root_expect_http_200(self):
-        url = reverse('microauth_authentication:index')
-        response = self.client.get(url)
-        self.assertEqual(200, response.status_code, 'Expect root view to load without issues.')
+        pipeline_settings = settings.PIPELINE
+        pipeline_settings['PIPELINE_ENABLED'] = False
+        with override_settings(PIPELINE_SETTINGS=pipeline_settings):
+            url = reverse('microauth_authentication:index')
+            response = self.client.get(url)
+            self.assertEqual(200, response.status_code, 'Expect root view to load without issues.')
