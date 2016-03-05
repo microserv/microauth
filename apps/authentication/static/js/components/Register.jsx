cb800a0bd936b7c8c2dc825314589d3f0d7a8ac5
sklirg <hlsolbjorg@gmail.com>
2016-03-05 16:42:38 +0000

    Enable signup from Reactjs app

--- apps/authentication/static/js/components/Register.jsx
+++ apps/authentication/static/js/components/Register.jsx
@@ -13,6 +13,40 @@ var Register = React.createClass({
     };
   },
 
+  getCookie: function getCookie(name) {
+    // https://docs.djangoproject.com/en/stable/ref/csrf/#ajax
+
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+      var cookies = document.cookie.split(';');
+      for (var i = 0; i < cookies.length; i++) {
+        var cookie = $.trim(cookies[i]);
+        // Does this cookie string begin with the name we want?
+        if (cookie.substring(0, name.length + 1) == (name + '=')) {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
+      }
+    }
+    return cookieValue;
+  },
+
+  prepareAjax: function prepareAjax() {
+    var csrftoken = this.getCookie('csrftoken');
+
+    function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
+  },
+
   handleEmailChange: function(event) {
     this.setState({
       email: event.target.value,
@@ -29,9 +63,29 @@ var Register = React.createClass({
   },
 
   handleSubmit: function() {
+    var state = this.state;
+
+    this.prepareAjax();
+
+    function handleSuccess(response) {
+      console.log(response);
+    }
+
+    function handleError(xhr, response, status) {
+      console.log(xhr, response, status);
+    }
+
     $.ajax({
-      url: '/api/'
-    })
+      url: '/api/users/',
+      method: 'POST',
+      data: {
+        username: state.username,
+        email: state.email,
+        password: state.password
+      },
+      success: handleSuccess,
+      error: handleError
+    });
   },
 
   render: function () {
