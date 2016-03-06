10f0d9c7336124bde65ef9add7d664a9b762eb11
sklirg <hlsolbjorg@gmail.com>
2016-03-06 22:04:24 +0000

    Add response debug for registration.

--- apps/authentication/static/js/components/Register.jsx
+++ apps/authentication/static/js/components/Register.jsx
@@ -8,7 +8,8 @@ var Register = React.createClass({
     return {
       email: '',
       password: '',
-      passwordRepeat:''
+      passwordRepeat:'',
+      response: ''
     };
   },
 
@@ -26,15 +27,18 @@ var Register = React.createClass({
 
   handleSubmit: function() {
     var state = this.state;
+    var that = this;
 
     this.props.prepareAjax();
 
     function handleSuccess(response) {
       console.log(response);
+      that.setState({ response: response });
     }
 
     function handleError(xhr, response, status) {
       console.log(xhr, response, status);
+      that.setState({ response: xhr });
     }
 
     $.ajax({
@@ -64,8 +68,13 @@ var Register = React.createClass({
           handlePasswordChange={this.handlePasswordChange}
           passwordRepeat={this.state.passwordRepeat}
           handlePasswordRepeatChange={this.handlePasswordRepeatChange}
-          handleSubmit={this.handleSubmit}
-        />
+          handleSubmit={this.handleSubmit} />
+        <div hidden={!this.state.response}>
+          <h3>Response</h3>
+          <pre>
+            {JSON.stringify(this.state.response, null, 2)}
+          </pre>
+        </div>
       </div>
     )
   }
