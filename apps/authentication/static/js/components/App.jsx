de4173d6480e7f5ffbc97861ec21e4626c287ffc
sklirg <hlsolbjorg@gmail.com>
2016-03-06 17:13:37 +0000

    Make the app look nice.

--- apps/authentication/static/js/components/App.jsx
+++ apps/authentication/static/js/components/App.jsx
@@ -4,6 +4,20 @@ var Login = require('./Login.jsx');
 var Register = require('./Register.jsx');
 
 var App = React.createClass({
+  getInitialState: function() {
+    return {
+      hideLogin: false,
+      hideRegistration: true
+    };
+  },
+
+  switchView: function() {
+    this.setState({
+      hideLogin: !this.state.hideLogin,
+      hideRegistration: !this.state.hideRegistration
+    });
+  },
+
   render: function () {
     return (
       <div>
@@ -11,13 +25,21 @@ var App = React.createClass({
           <h1>MicroAuth</h1>
           <h3>Authentication service for microserv</h3>
         </hgroup>
-        <div className="split-page left">
+        <div className="" hidden={this.state.hideLogin}>
           <Login />
         </div>
-        <div className="split-page right">
+        <div className="" hidden={this.state.hideRegistration}>
           <Register />
         </div>
         <div className="clearfix"></div>
+        <hgroup>
+          <h3 onClick={this.switchView} hidden={this.state.hideLogin}>
+            Need an account? Click here to sign up.
+          </h3>
+          <h3 onClick={this.switchView} hidden={this.state.hideRegistration}>
+            Already have an account? Click here to log in.
+          </h3>
+        </hgroup>
       </div>
     )
   }
