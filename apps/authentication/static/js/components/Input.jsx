aea43270c67bb06d8d42115b0b2948fb206c295c
sklirg <hlsolbjorg@gmail.com>
2016-03-06 16:49:27 +0000

    Trigger css-update if autofilled by browser.

--- apps/authentication/static/js/components/Input.jsx
+++ apps/authentication/static/js/components/Input.jsx
@@ -1,21 +1,36 @@
 var React = require('react');
 
 var LoginForm = React.createClass({
-  handleFocus: function(event, target) {
-    if (event.target.value) {
-      event.target.className = 'used';
+  getInitialState: function() {
+    return {
+      className: ''
+    };
+  },
+
+  updateFocusState: function(elem) {
+    if (elem.value) {
+      this.setState({ className: 'used' });
     } else {
-      event.target.className = '';
+      this.setState({ className: '' });
     }
+  },
+
+  handleFocus: function(event) {
+    this.updateFocusState(event.target);
+  },
+
+  handleChange: function(event) {
+    this.props.onChange(event);
+    this.updateFocusState(event.target);
   },
 
   render: function () {
     return (
-      <input type={this.props.type} name={this.props.name} className={this.props.className}
-             value={this.props.value} onChange={this.props.onChange}
+      <input type={this.props.type} name={this.props.name} className={this.state.className}
+             value={this.props.value} onChange={this.handleChange}
              onFocus={this.handleFocus} onBlur={this.handleFocus} />
     )
   }
-})
+});
 
 module.exports = LoginForm;
