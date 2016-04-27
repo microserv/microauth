66c4ac9a4c5badf97ef21b9fcbf3254f8326695a
Pål Karlsrud <paulkarlsrud@gmail.com>
2016-04-27 07:37:47 -0400

    Revert "Move the admin page to the oauth2 subsite."
    As this is not really needed after all.
    
    This reverts commit bacc55fd0125494477cbcb7f2ae32f2be5fe70cf.

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
-from django.contrib import admin
 
 from apps.authentication.views import verify
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='authentication/index.html'), name='index'),
-    url(r'^admin/', admin.site.urls),
     url(r'verify/$', verify, name='token-verify')
 ]
